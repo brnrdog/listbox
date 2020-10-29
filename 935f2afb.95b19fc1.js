943ffda319f14a43c780cf5f9d5a36e4607dff16
Bernardo Gurgel <brnrdog@hey.com>
2020-10-29 19:32:00 +0100

    Deploy website - based on dced8bbc646abe882103b932b0960b5d389914bf

--- 935f2afb.95b19fc1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{62:function(e){e.exports=JSON.parse('{"version":"current","label":"Next","isLast":true,"docsSidebars":{"docs":[{"type":"link","label":"Getting started","href":"/docs/"},{"type":"link","label":"Examples","href":"/docs/examples"}]},"permalinkToSidebar":{"/docs/examples":"docs","/docs/":"docs"}}')}}]);
